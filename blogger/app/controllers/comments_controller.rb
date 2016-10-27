99ad06ace56e429437b7f538cc57d3763bb35de5
Valerie Hager <vhager2411@gmail.com>
2016-10-26 21:48:09 -0700

    tagging feature nearly complete

--- blogger/app/controllers/comments_controller.rb
+++ blogger/app/controllers/comments_controller.rb
@@ -2,11 +2,11 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.article_id = params[:article.id]
+    @comment.article_id = params[:article_id]
 
     @comment.save
 
-    redirect_to article_path(@comment.artcile)
+    redirect_to article_path(@comment.article)
   end
 
   def comment_params
